9b33d53258235ba4134ce4fa08d6e21f379b83d9
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-02-07 11:56:45 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 4032 ms, Memory: 843216 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -0,0 +1,29 @@
+# 최솟값 찾기
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, L = map(int,input().split())
+arr = list(map(int,input().split()))
+
+answer = []
+Q = deque()
+
+for i in range(L):
+    while Q and Q[-1][0] >= arr[i]:
+        Q.pop()
+    
+    Q.append([arr[i], i])
+    answer.append(Q[0][0])
+
+for i in range(L,N):
+    while Q and Q[-1][0] >= arr[i]:
+        Q.pop()
+    
+    Q.append([arr[i], i])
+    if Q[0][1] == (i-L):
+        Q.popleft()
+    
+    answer.append(Q[0][0])
+
+print(*answer)
