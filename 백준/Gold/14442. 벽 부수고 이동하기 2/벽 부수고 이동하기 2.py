c3d7bcc9ee27527e87ebd364d928a87f5ce34af1
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-02-14 00:29:22 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 2440 ms, Memory: 221052 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -6,7 +6,7 @@ N, M, K = map(int,input().split())  # N, M, K input
 mapp = [list(input().rstrip()) for __ in range(N)]
 
 dd = ((0,1),(0,-1),(1,0),(-1,0))
-visited = [[sys.maxsize for __ in range(M)] for __ in range(N)]
+visited = [[K+1 for __ in range(M)] for __ in range(N)]
 
 answer = -2
 
@@ -16,13 +16,12 @@ def in_range(r, c):
 def bfs():
     global answer
 
-    Q = deque([(0, 0, 0)])
+    Q = deque([(0, 0)])
     visited[0][0] = 0
     count = 0
     while Q:
-
         for _ in range(len(Q)):
-            r, c, k = Q.popleft()
+            r, c = Q.popleft()
             if r == N-1 and c == M-1:
                 answer = count
                 Q = []
@@ -32,14 +31,14 @@ def bfs():
                 nr, nc = r+dd[i][0], c+dd[i][1]
                 if not in_range(nr, nc): continue
                 if mapp[nr][nc] == '1':
-                    if k < K and visited[nr][nc] > k+1:
-                        visited[nr][nc] = k+1
-                        Q.append((nr, nc, k+1))
+                    if visited[r][c] < K and visited[nr][nc] > visited[r][c]+1:
+                        visited[nr][nc] = visited[r][c]+1
+                        Q.append((nr, nc))
                 
                 if mapp[nr][nc] == '0':
-                    if visited[nr][nc] > k:
-                        visited[nr][nc] = k
-                        Q.append((nr, nc, k))
+                    if visited[nr][nc] > visited[r][c]:
+                        visited[nr][nc] = visited[r][c]
+                        Q.append((nr, nc))
 
         count += 1
 
