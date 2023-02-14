d023bdac272a252f5aae4cc61167df4d7b377aa8
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-02-14 18:47:04 +0900

    [Platinum V] Title: 찾기, Time: 772 ms, Memory: 82224 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.py
+++ 백준/Platinum/1786. 찾기/찾기.py
@@ -1,54 +1,55 @@
 import sys
 input = sys.stdin.readline
 
-def next_start(P):
-    # O(m) !
-    return_list = [0 for __ in range(m+1)]
-    k = 0
-    for i in range(1,m+1):
-        return_list[i] = k
-        if i >= m:
-            break
-        if P[k] == P[i]:
-            k += 1
-        else:
-            k = return_list[k]
-            if P[k] != P[i]:
-                k = 0
-            else:
-                k += 1
-            
-    return return_list
-
-
-
 T = input()[:-1]
-P = input()[:-1]
-# T = input()
-# P = input()
+P = input()
 
-n, m = len(T), len(P)
+if T[-1] == '\n':
+    T = T[:-1]
+if P[-1] == '\n':
+    P = P[:-1]
 
-count = 0
-loc = []
+dp = [0 for __ in range(len(P))]
 
-nexts = next_start(P)
-i = 0
-s = 0
-while i <= n-m:
-    for j in range(s, m):
-        if T[i+j] != P[j]:
-            break
-        if j == m-1:
+def make_function():
+    j = 0
+    for i in range(1, len(P)):
+        while j > 0 and P[i] != P[j]:
+            j = dp[j-1]
+        if P[i] == P[j]:
             j += 1
-            
-    if j == m:
-        count += 1
-        loc.append(i+1)
-    
-    i += max(j-nexts[j],1)
-    s = nexts[j]
-    
-print(count)
-if loc:
-    print(*loc)
+            dp[i] = j
+        
+
+answer = 0
+answers = []
+def find(T,P):
+    global answer
+    s = 0
+    i = 0
+
+    while s+i < len(T):
+        if T[s+i] == P[i]:
+            i += 1
+
+        elif i == 0:
+            s += 1
+            i = 0
+
+        else:
+            next_i = dp[i-1]          # 몇 개까지 일치하는지
+            s = s + i - next_i        # 일치하는 갯수만큼 뒤에서 시작
+            i = next_i
+        
+        if i == len(P):
+            answer += 1
+            answers.append(s+1)
+
+            next_i = dp[i-1]
+            s = s + i-next_i 
+            i = next_i
+
+make_function()
+find(T, P)
+print(answer)
+print(*answers)
