fae9dd03b643735204b6818af13e68a2c65f045e
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-03-17 15:44:16 +0900

    [Gold IV] Title: 좋다, Time: 1360 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,28 @@
+N = int(input())
+arr = list(map(int,input().split()))
+arr.sort()
+
+answer = 0
+
+
+for i in range(N):
+    l, r = 0, N-1
+
+    target = arr[i]
+    while l < r:
+        if l == i:
+            l += 1
+            continue
+        if r == i:
+            r -= 1
+            continue
+
+        if arr[l]+arr[r] == target:
+            answer += 1
+            break
+        elif arr[l]+arr[r] > target:
+            r -= 1
+        else:
+            l += 1
+
+print(answer)
