9e9a4cf8fad6e9347fbdc481f2ff4b0ee75a609c
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-10-13 18:31:07 +0900

    [Gold V] Title: 자두나무, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.py
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -1,36 +1,20 @@
-# 2차원 dp면 될거 같다.
-T, W = map(int, input().rstrip().split())
+T, W = map(int, input().split())
 drops = [int(input()) for _ in range(T)]
 
-# dp[m][t][loc] : m 번째 움직였을 때, t번째에 loc 인덱스의 열매를 먹었을 때, 얻을 수 있는 최대 값
-dp = [[[0 for _ in range(2)] for _ in range(T + 2)] for _ in range(W + 1)]
-for m in range(0, W + 1):
-    for t in range(T - 1, -1, -1):
-        if drops[t] == 1:
-            if m == 0:
-                dp[m][t][0] = dp[m][t + 1][0] + 1
-                dp[m][t][1] = dp[m][t + 1][1]
-                continue
 
-            dp[m][t][0] = max(
-                dp[m][t + 1][0] + 1,
-                dp[m - 1][t + 1][1] + 1,
-                # dp[m - 1][t + 1][0] + 1,
-            )
-            dp[m][t][1] = max(dp[m][t + 1][1], dp[m - 1][t + 1][1])
+dp = [[0 for _ in range(T + 1)] for _ in range(2)]
+for moving in range(W + 1):
+    for t in range(1, T + 1):
+        if moving % 2 == 0:
+            if drops[t - 1] == 1:
+                dp[0][t] = max(dp[0][t - 1] + 1, dp[1][t - 1] + 1)
+            else:
+                dp[0][t] = dp[0][t - 1]
 
-        elif drops[t] == 2:
-            if m == 0:
-                dp[m][t][0] = dp[m][t + 1][0]
-                dp[m][t][1] = dp[m][t + 1][1] + 1
-                continue
+        else:
+            if drops[t - 1] == 2:
+                dp[1][t] = max(dp[1][t - 1] + 1, dp[0][t - 1] + 1)
+            else:
+                dp[1][t] = dp[1][t - 1]
 
-            dp[m][t][1] = max(
-                dp[m][t + 1][1] + 1,
-                dp[m - 1][t + 1][0] + 1,
-                # dp[m - 1][t + 1][1] + 1,
-            )
-            dp[m][t][0] = max(dp[m][t + 1][0], dp[m - 1][t + 1][0])
-
-
-print(max(dp[W][0][0], dp[W - 1][0][1]))
+print(max(dp[0][T], dp[1][T]))
