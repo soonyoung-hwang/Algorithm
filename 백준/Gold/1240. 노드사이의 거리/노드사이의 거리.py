7aba325fa4f5d59c1dac88dd9dbcea188c1df4b0
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-10-20 20:11:08 +0900

    [Gold V] Title: 노드사이의 거리, Time: 348 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -0,0 +1,28 @@
+def find_cost(a, b):
+    visited = [False for _ in range(N + 1)]
+    stack = [(a, 0)]
+
+    while stack:
+        cur, cur_cost = stack.pop()
+        if cur == b:
+            return cur_cost
+
+        visited[cur] = True
+
+        for next_node, added_cost in routes[cur]:
+            if visited[next_node]:
+                continue
+
+            stack.append((next_node, cur_cost + added_cost))
+
+
+N, M = map(int, input().split())
+routes = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b, cost = map(int, input().split())
+    routes[a].append([b, cost])
+    routes[b].append([a, cost])
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    print(find_cost(a, b))
