23ab20a155ba19e9178d4e89ed2559a281ceeffa
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-10-20 13:55:53 +0900

    [Gold III] Title: 구간 나누기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2228. 구간 나누기/구간 나누기.py
+++ 백준/Gold/2228. 구간 나누기/구간 나누기.py
@@ -1,45 +1,17 @@
-def find_max(i, j):
-    temp_max = numbers[i]
-    temp_sum = numbers[i]
+# 백준 2228 다른사람 코드 (정해)
 
-    for number in numbers[i + 1 : j + 1]:
-        if temp_sum < 0:
-            if temp_sum < number:
-                temp_sum = number
-        elif temp_sum + number < 0:
-            temp_sum = 0  # 모두 뺀다.
+import sys
 
-        else:
-            temp_sum += number
+input = sys.stdin.readline
 
-        temp_max = max(temp_max, temp_sum)
+n, m = map(int, input().split())
+con = [[0] + [-1e9] * m for i in range(n + 1)]
+notcon = [[0] + [-1e9] * m for i in range(n + 1)]
 
-    return temp_max
+for i in range(1, n + 1):
+    num = int(input())
+    for j in range(1, m + 1):
+        notcon[i][j] = max(con[i - 1][j], notcon[i - 1][j])
+        con[i][j] = max(con[i - 1][j], notcon[i - 1][j - 1]) + num
 
-
-N, M = map(int, input().split())
-numbers = [int(input()) for _ in range(N)]
-
-# dp[i][j][k] : i 개의 구간으로 나누어져있을 때, i번째 숫자부터 j번째 숫자까지의 최대값
-dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(M + 1)]
-
-# M = 1일 때 직접 구한다.
-for i in range(N):
-    for j in range(i, N):
-        dp[1][i][j] = find_max(i, j)
-
-
-# 나머지는 dp로 구해준다.
-for m in range(2, M + 1):
-    for i in range(N):
-        for j in range(i, N):
-            temp_max = -33000 * 50
-
-            for dif in range(j - i - 1):
-                if dp[1][i][i + dif] == -33000 or dp[m - 1][i + dif + 2][j] == -33000:
-                    continue
-                temp_max = max(temp_max, dp[1][i][i + dif] + dp[m - 1][i + dif + 2][j])
-
-            dp[m][i][j] = temp_max
-
-print(dp[M][0][N - 1])
+print(max(con[n][m], notcon[n][m]))
