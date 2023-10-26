014f5992f6075059bfa2811cf1d8bb0830e597b6
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-10-26 18:56:01 +0900

    [Gold V] Title: 택배 배송, Time: 312 ms, Memory: 54296 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,31 @@
+# 풀이 : Dijkstra using cost update
+from heapq import heappush, heappop
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+routes = defaultdict(list)
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    routes[a].append((b, c))
+    routes[b].append((a, c))
+
+costs = [50_000_001 for _ in range(N + 1)]
+costs[1] = 0
+Q = [(0, 1)]
+
+while Q:
+    cost, node = heappop(Q)
+    if node == N:
+        answer = cost
+        break
+
+    for _next, add in routes[node]:
+        if costs[_next] <= cost + add:
+            continue
+        costs[_next] = cost + add
+        heappush(Q, (cost + add, _next))
+
+
+print(answer)
