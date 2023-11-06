4aa373f9c2f0f234176636b24db38d9289709068
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2023-11-06 11:25:44 +0900

    [Gold V] Title: LCS, Time: 600 ms, Memory: 56528 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,13 @@
+A = input().rstrip()
+B = input().rstrip()
+
+dp = [[0 for _ in range(len(A)+1)] for _ in range(len(B)+1)]
+
+for i in range(1, len(B)+1):
+    for j in range(1, len(A)+1):
+        if A[j-1] == B[i-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[len(B)][len(A)])
