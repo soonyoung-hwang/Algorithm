5e2ca0c0bdb8c3b9a216a5c20d1323c4299da464
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2024-03-21 19:37:07 +0900

    [level 3] Title: 주사위 고르기, Time: 2457.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/README.md
+++ 프로그래머스/3/258709. 주사위 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 주사위 고르기 - 258709 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2541.09 ms
+메모리: 10.3 MB, 시간: 2457.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 22:0:47
+2024년 03월 21일 19:37:00
 
 ### 문제 설명
 
--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -2,33 +2,51 @@ from itertools import combinations
 from bisect import bisect_left, bisect_right
 
 def solution(dice):
-    answer = []
-    for i in range(len(dice)):
-        dice[i].sort()
+    # answer = []
+    L = len(dice)
+    def new_counter_parts():
+        return tuple([i for i in range(L)])
     
-    print(dice)
-    
-    
-#     def new_counter_parts():
-#         return tuple([i for i in range(len(dice))])
-    
-#     def calculate_score(combi_a, combi_b):
+    def cal_value(calculated, combi, depth, max_depth, stack):
+        if depth == max_depth:
+            stack.append(calculated)
+            return
         
+        for i in range(6):
+            calculated += dice[combi[depth]][i]
+            cal_value(calculated, combi, depth+1, max_depth, stack)
+            calculated -= dice[combi[depth]][i]
     
-#     combis = combinations([i for i in range(len(dice))], len(dice)//2)
-#     done = set()
+    def cal_diff(combi_a, combi_b):
+        count = 0
+        stack_a = []
+        stack_b = []
+        
+        cal_value(0, combi_a, 0, L//2, stack_a)
+        cal_value(0, combi_b, 0, L//2, stack_b)
+        
+        stack_b.sort()
+        for num in stack_a:
+            count += bisect_left(stack_b, num)
+        
+        return count
     
-#     for combi in combis:
-#         counter_parts = set(new_counter_parts())
-#         counter_parts -= set(combi)
-        
-#         if frozenset(combi) in done:
-#             continue
-        
-#         done.add(frozenset(combi))
-#         done.add(frozenset(counter_parts))
-        
-#         diff = calculate_score(combi, tuple(counter_parts))
+    combis = combinations([i for i in range(L)], L//2)    
+    best_score = 0
 
+    for combi in combis:
+        counter_parts = set(new_counter_parts())
+        counter_parts -= set(combi)
+        
+        score = cal_diff(list(combi), list(counter_parts))
+        
+        if best_score < score:
+            answer = list(combi)[:]
+            best_score = score
+        
+    for i in range(len(answer)):
+        answer[i] += 1
+    
+    answer.sort()
     
     return answer
