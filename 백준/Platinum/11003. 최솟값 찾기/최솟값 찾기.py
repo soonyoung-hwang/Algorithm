bd3ffecab1f93e6839133291f4c10b2945ab34df
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2024-10-11 20:20:59 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 3264 ms, Memory: 712056 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -13,14 +13,14 @@ for i in range(L):
     while Q and Q[-1][0] >= arr[i]:
         Q.pop()
     
-    Q.append([arr[i], i])
+    Q.append((arr[i], i))
     answer.append(Q[0][0])
 
 for i in range(L,N):
     while Q and Q[-1][0] >= arr[i]:
         Q.pop()
     
-    Q.append([arr[i], i])
+    Q.append((arr[i], i))
     if Q[0][1] == (i-L):
         Q.popleft()
     
