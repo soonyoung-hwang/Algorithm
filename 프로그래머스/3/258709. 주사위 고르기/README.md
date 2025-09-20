a1a9f7fa110aaad48cac2a121d76d17ab5e15a46
Soonyoung <78343941+soonyoung-hwang@users.noreply.github.com>
2025-09-20 21:15:12 +0900

    [level 3] Title: 주사위 고르기, Time: 1133.86 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/README.md
+++ 프로그래머스/3/258709. 주사위 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 주사위 고르기 - 258709 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 2457.55 ms
+메모리: 9.97 MB, 시간: 1133.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 21일 19:37:00
+2025년 09월 20일 21:15:07
 
 ### 문제 설명
 
--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,52 +1,71 @@
-from itertools import combinations
-from bisect import bisect_left, bisect_right
+from collections import deque
 
 def solution(dice):
-    # answer = []
-    L = len(dice)
-    def new_counter_parts():
-        return tuple([i for i in range(L)])
+    answer = []
     
-    def cal_value(calculated, combi, depth, max_depth, stack):
-        if depth == max_depth:
-            stack.append(calculated)
+    cases = []
+    stack = []
+    
+    def dfs(idx, L):
+        if len(stack) == L/2:
+            cases.append(stack[:])
             return
-        
-        for i in range(6):
-            calculated += dice[combi[depth]][i]
-            cal_value(calculated, combi, depth+1, max_depth, stack)
-            calculated -= dice[combi[depth]][i]
-    
-    def cal_diff(combi_a, combi_b):
-        count = 0
-        stack_a = []
-        stack_b = []
-        
-        cal_value(0, combi_a, 0, L//2, stack_a)
-        cal_value(0, combi_b, 0, L//2, stack_b)
-        
-        stack_b.sort()
-        for num in stack_a:
-            count += bisect_left(stack_b, num)
-        
-        return count
-    
-    combis = combinations([i for i in range(L)], L//2)    
-    best_score = 0
+        if idx >= L:
+            return
 
-    for combi in combis:
-        counter_parts = set(new_counter_parts())
-        counter_parts -= set(combi)
-        
-        score = cal_diff(list(combi), list(counter_parts))
-        
-        if best_score < score:
-            answer = list(combi)[:]
-            best_score = score
-        
-    for i in range(len(answer)):
-        answer[i] += 1
+        stack.append(idx)
+        dfs(idx+1, L)
+        stack.pop()
+        dfs(idx+1, L)
     
-    answer.sort()
+    dfs(0, len(dice))
+    
+    def get_sums(target_list):
+        ret = deque([0])
+        target_list = deque(target_list)
+        
+        while target_list:
+            target = dice[target_list.popleft()]
+            for i in range(len(ret)):
+                num = ret.popleft()
+                for j in range(6):
+                    ret.append(num + target[j])
+        return ret
+        
+    
+    # 2. 각 조합마다 숫자 비교하기
+    max_point = 0
+    
+    cases = deque(cases)
+    while cases:
+        a = cases.popleft()
+        b = cases.pop()
+        
+        block_1, block_2 = list(get_sums(a)), list(get_sums(b))
+        block_1.sort()
+        block_2.sort()
+            
+        point = 0
+        s = 0
+        for num in block_1:
+            while s < len(block_1) and num > block_2[s]:
+                s += 1
+            point += s
+            
+        if point > max_point:
+            max_point = point
+            answer = [a[i]+1 for i in range(len(a))]
+            
+        point = 0
+        s = 0
+        for num in block_2:
+            while s < len(block_1) and num > block_1[s]:
+                s += 1
+            point += s
+        
+        if point > max_point:
+            max_point = point
+            answer = [b[i]+1 for i in range(len(b))]
     
     return answer
+
